6583c1e835a76e7e7f717a186f01a13f515a7ed7
Norillian <mic@scancommerce.dk>
2016-07-20 13:56:34 +0200

    Changes

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,31 @@ $(document).ready(function(){
 
         e.preventDefault();
         $('.content-wrapper').toggleClass('show-nav');
-    
+
     });
+
+    //Change item amount in basket detailed
+    if($('#BasketdetailedBoxbdy .c1').length > 0) {
+        currentTxt = $('#BasketdetailedBoxbdy .c1').text();
+        currentTxt = currentTxt.replace('Antal varer:', '').replace(/\s/g, '');
+        console.log(currentTxt);
+        $('#BasketdetailedBoxbdy').prepend('<span class="item-amount">'+currentTxt+'</span>');
+    }
+
+    //Create the menus in Off-Canvas
+    if($('#ddMenu').length > 0) {
+
+        offcanvasMenu = $('<ul></ul>');
+        offcanvasMenu.addClass('info-menu-left');
+
+        $( "#ddMenu span" ).each(function( index ) {
+            offcanvasMenuList = $('<li></li>');
+            offcanvasMenuList.append($(this).html());
+            offcanvasMenu.append(offcanvasMenuList);
+        });
+
+        $('.off-canvas-menu').append(offcanvasMenu);
+
+    }
 
 });
